c137ff72103b668e9f6a0edf9b9ee46c3cb43baa
Aditya <Adityadeolalikar19@gmail.com>
2025-02-16 13:33:47 +0530

    Signupandlogin

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from 'next/font/local'
-
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${openDyslexic.className} antialiased`}
       >
-        
+        <Navbar/>
         {children}
       </body>
     </html>
